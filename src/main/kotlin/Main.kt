5d8e2088b4f7f60b30b930d4a7c5fb1e206c4030
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-07 21:23:35 +0100

    test(list): provide test for ArrayList based queue

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
+import list.ArrayListQueue
+
 fun main() {
 
+    "Queue with ArrayList" example {
+        val queue = ArrayListQueue<String>().apply {
+            enqueue("Ray")
+            enqueue("Brian")
+            enqueue("Eric")
+        }
+        println(queue)
+        queue.dequeue()
+        println(queue)
+        println("Next up: ${queue.peek()}")
+    }
+
+}
+
+infix fun String.example(function: () -> Unit) {
+    println("\n=== Example of $this ===")
+    function()
 }
