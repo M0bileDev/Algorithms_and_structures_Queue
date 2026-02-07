10f3644fe5e8c597fd43300121e901c13a77acce
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-07 22:40:35 +0100

    feat(ringbuffer): override toString function

--- src/main/kotlin/ringbuffer/RingBufferQueue.kt
+++ src/main/kotlin/ringbuffer/RingBufferQueue.kt
@@ -12,9 +12,13 @@ class RingBufferQueue<T : Any>(size: Int) : Queue<T> {
     }
 
     override fun dequeue(): T? {
-       return if(isEmpty) null else ringBuffer.read()
+        return if (isEmpty) null else ringBuffer.read()
     }
 
     override fun peek(): T? = ringBuffer.first
+
+    override fun toString(): String {
+        return ringBuffer.toString()
+    }
 
 }
