6ea5a3ff93d7570d4ad02eaab12b31baa42e443c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-07 20:59:48 +0100

    feat(list): implement count and peek
    Provided the size of the queue, provided element at the front of the queue

--- src/main/kotlin/list/ArrayListQueue.kt
+++ src/main/kotlin/list/ArrayListQueue.kt
@@ -2,6 +2,11 @@ package list
 
 import Queue
 
-class ArrayListQueue<T: Any>: Queue<T>{
+class ArrayListQueue<T : Any> : Queue<T> {
     private val list = arrayListOf<T>()
+
+    override val count: Int
+        get() = list.size
+
+    override fun peek(): T? = list.getOrNull(0)
 }
