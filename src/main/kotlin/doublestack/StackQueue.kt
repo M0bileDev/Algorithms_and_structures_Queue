041292468241edceb211334d7c50b59a9105d7ee
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-08 21:20:49 +0100

    feat(doublestack): provide stack queue variant of queue

---
+++ src/main/kotlin/doublestack/StackQueue.kt
@@ -0,0 +1,21 @@
+package doublestack
+
+import Queue
+
+class StackQueue<T: Any> : Queue<T>{
+    override val count: Int
+        get() = TODO("Not yet implemented")
+
+    override fun enqueue(element: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun dequeue(): T? {
+        TODO("Not yet implemented")
+    }
+
+    override fun peek(): T? {
+        TODO("Not yet implemented")
+    }
+
+}
