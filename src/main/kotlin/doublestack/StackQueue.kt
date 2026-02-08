cab0ca016038f61af958581398b67f48a9c1d395
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-08 21:27:35 +0100

    feat(doublestack): provide stack interface

--- src/main/kotlin/doublestack/StackQueue.kt
+++ src/main/kotlin/doublestack/StackQueue.kt
@@ -2,6 +2,19 @@ package doublestack
 
 import Queue
 
+interface Stack<T : Any> {
+    fun push(element: T)
+
+    fun pop(): T?
+
+    fun peek(): T?
+
+    val count: Int
+
+    val isEmpty: Boolean
+        get() = count == 0
+}
+
 class StackQueue<T: Any> : Queue<T>{
     override val count: Int
         get() = TODO("Not yet implemented")
