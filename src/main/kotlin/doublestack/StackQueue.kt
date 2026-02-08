96c3a785d55db0320018454213e32a0cd777f72c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-08 21:27:59 +0100

    feat(doublestack): provide stack implementation

--- src/main/kotlin/doublestack/StackQueue.kt
+++ src/main/kotlin/doublestack/StackQueue.kt
@@ -15,6 +15,50 @@ interface Stack<T : Any> {
         get() = count == 0
 }
 
+class StackImpl<T : Any> : Stack<T> {
+    private val storage = arrayListOf<T>()
+
+    override fun toString() = buildString {
+        appendLine("----top----")
+        storage.asReversed().forEach {
+            appendLine("$it")
+        }
+        appendLine("-----------")
+    }
+
+    override fun push(element: T) {
+        storage.add(element)
+    }
+
+    override fun pop(): T? {
+        if (isEmpty) {
+            return null
+        }
+        return storage.removeAt(count - 1)
+    }
+
+    override fun peek(): T? {
+        return storage.lastOrNull()
+    }
+
+    override val count: Int
+        get() = storage.size
+
+    companion object {
+        fun <T : Any> create(items: Iterable<T>): Stack<T> {
+            val stack = StackImpl<T>()
+            for (item in items) {
+                stack.push(item)
+            }
+            return stack
+        }
+    }
+}
+
+fun <T : Any> stackOf(vararg elements: T): Stack<T> {
+    return StackImpl.create(elements.asList())
+}
+
 class StackQueue<T: Any> : Queue<T>{
     override val count: Int
         get() = TODO("Not yet implemented")
